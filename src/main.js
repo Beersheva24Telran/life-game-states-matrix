35196161b364109eb661ac8b2a963f8fb1842d59
Yuragranovsky <yuragranovsky@gmail.com>
2025-04-29 16:24:32 +0300

    final solution of life game states matrix

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import configObj from './config/matrix-config.json'
 import MatrixStates from './service/MatrixStates';
 const {rows, columns, interval} = configObj;
 
-new MatrixComponent(rows, columns, document.querySelector(".grid"), new MatrixStates());
+new MatrixComponent(rows, columns, document.querySelector(".grid"), new MatrixStates(), interval);
 
--- src/service/MatrixStates.js
+++ src/service/MatrixStates.js
@@ -10,7 +10,32 @@ export default class MatrixStates {
     return this.#_matrix;
   }
   next(){
-    this.#_matrix = this.createRandomMatrix(this.#_matrix.length, this.#_matrix[0].length);
+    this.#_matrix = this.#_matrix.map((row, rIndex) => this.#newRow(row, rIndex));
     return this.#_matrix;
   }
+  #newRow(row, rIndex) {
+    return row.map((cell, cIndex) => this.#newCell(cell, rIndex, cIndex));
+  }
+  #newCell(cell, rIndex, cIndex) {
+    this.#_matrix[rIndex][ cIndex] = 0; //not to count for neighbors
+    const neighbors = this.#getNeighborsArea(rIndex, cIndex);
+    const sumNeighbors = neighbors.flatMap(a => a).reduce((sum, cur) => sum + cur);
+    const res = cell ? forPopulated(sumNeighbors) : forUnpopulated(sumNeighbors);
+    this.#_matrix[rIndex] [cIndex] = cell; //restoring the current cell
+    return res;
+
+  }
+  #getNeighborsArea(rIndex, cIndex){
+   
+    const startIndex = cIndex === 0 ? cIndex : cIndex -1;
+    const endIndex = cIndex === this.#_matrix[0].length - 1 ? cIndex + 1 : cIndex + 2;
+    return [rIndex - 1, rIndex, rIndex + 1].map(i => this.#_matrix[i] ?
+         this.#_matrix[i].slice(startIndex, endIndex) : [0]);
+  }
+}
+function forPopulated(sumNeighbors) {
+    return +(sumNeighbors === 2 || sumNeighbors === 3);
+}
+function forUnpopulated(sumNeighbors) {
+    return +(sumNeighbors === 3);
 }
